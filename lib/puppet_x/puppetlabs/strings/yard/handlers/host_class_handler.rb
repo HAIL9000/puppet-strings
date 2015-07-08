9e45ea3b585a7fa58a501a109d1205594aefa492
Ian Kronquist <iankronquist@teleport.com>
2015-07-08 16:12:04 -0700

    (PDOC-19) Typecheck documented parameters
    The documented parameter types should be compared with the actual types when
    possible.

--- lib/puppet_x/puppetlabs/strings/yard/handlers/host_class_handler.rb
+++ lib/puppet_x/puppetlabs/strings/yard/handlers/host_class_handler.rb
@@ -8,6 +8,21 @@ class PuppetX::PuppetLabs::Strings::YARD::Handlers::HostClassHandler < PuppetX::
         param_tuple << ( a[1].nil? ? nil : a[1].source )
       end
     end
+    tp = Puppet::Pops::Types::TypeParser.new
+    param_type_info = []
+    # Ensure that this pop object has parameters
+    if statement.pops_obj.respond_to? :parameters
+      param_type_info = {}
+      statement.pops_obj.parameters.each do |pop_param|
+        begin
+          param_type_info[pop_param.name] =  tp.interpret_any(pop_param.type_expr)
+        rescue Puppet::ParseError
+          # If the type could not be interpreted, default to type Any
+          param_type_info[pop_param.name] =  tp.parse('Any')
+        end
+      end
+    end
+    obj.type_info = [param_type_info]
 
     statement.pops_obj.tap do |o|
       if o.parent_class
